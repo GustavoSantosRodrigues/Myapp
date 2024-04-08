117dc8dc55fe8d6e33285eaab1d8339ab41eb9cb
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2024-04-08 19:00:09 -0300

    21. App Biscoito da Sorte

--- App.js
+++ App.js
@@ -1,42 +1,53 @@
 // SERVE PARA CRIAR UM COMPONENTE NO REACT NATIVE
 
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button, Image, TouchableOpacity } from "react-native";
 
 
 // const nome = 'Gustavo'  
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      nome: '',
-      input: ''
-    }
-    this.entrar = this.entrar.bind(this)
+      textoFrase:  'Aproveite o biscoito da sorte',
+      img: require('./src/biscoito.png'),
+    };
+
+    this.textoQuebraBiscoito = this.textoQuebraBiscoito.bind(this);
+
+    this.frase = [
+      'Aproveite o biscoito da sorte',
+      'Siga os bons e aprenda  com eles',
+      "Acredite em si mesmo. Você é mais capaz do que imagina.",
+      "O sucesso não é o resultado de um acaso, mas sim de uma determinação inabalável.",
+      "Cada novo dia é uma oportunidade para se aproximar dos seus sonhos. Não desperdice.",
+      "As dificuldades de hoje são os degraus para o seu sucesso amanhã.",
+      "Seja a mudança que você deseja ver no mundo. - Mahatma Gandhi",
+      "Não importa o quão devagar você vá, desde que você não pare.",
+      "Você é mais forte do que pensa, mais corajoso do que acredita e mais talentoso do que imagina. - A.A. Milne",
+      "O único lugar onde o sucesso vem antes do trabalho é no dicionário. - Vidal Sassoon",
+      "Nunca é tarde demais para ser quem você quer ser. Não há limite de tempo, comece quando quiser.",
+    ]
   }
 
-  entrar(){
-    if(this.state.input === ''){
-      alert('digite seu nome')
-      return
-    }
+  textoQuebraBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * this.frase.length)
     this.setState({
-      nome: 'Bem Vindo: ' + this.state.input
+      textoFrase: ' "' + this.frase[numeroAleatorio] + '" ',
+      img: require('./src/biscoitoAberto.png')
     })
-  
   }
 
   render() {
     return (
       <View style={styles.container} >
-        <TextInput style={styles.input}
-          placeholder="digite seu nome"
-          underlineColorAndroid="transparent"
-          onChangeText={(texto) => this.setState({ input: texto })}
-        />
-        <Button title="entrar" onPress={this.entrar} />
-        <Text style={styles.name}>{this.state.nome}</Text>
+        <Image source={this.state.img} style={styles.img} />
+        <Text style={styles.textoFrase}>{this.state.textoFrase} </Text>
+        <TouchableOpacity onPress={this.textoQuebraBiscoito}>
+          <View>
+            <Text style={styles.textoQuebraBiscoito}>Quebra o biscoito</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -50,84 +61,33 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  input: {
-    borderWidth: 1,
-    borderColor: '#000',
-    height: 40,
-    width: 300,
-    paddingLeft: 10,
-    color: '#000',
-    marginBottom: 10
+  img: {
+    width: 250,
+    height: 250
   },
 
-  name: {
+  textoFrase: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+
+  textoQuebraBiscoito: {
     fontSize: 25,
     color: '#000',
-    fontWeight: 'bold',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center',
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25,
   },
+
+
 })
 
 export default App
-
-// state e onPress
-
-// class App extends Component {
-
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       nome: ''
-//     }
-//     this.entrar = this.entrar.bind(this)
-//     // deixa a funcao acessar todas as propriedades
-
-//     // state consegue trocar o valor dela 
-//   }
-
-//   entrar(nome){
-//     this.setState(
-//       {nome: nome}
-//     )
-//   }
-//   render() {
-//     return (
-//     <View style={styles.container}>
-
-//       <Button title="entrar" onPress={() => this.entrar('Gustavo')} />
-//       <Text>{this.state.nome}</Text>
-//     </View>
-//     )
-//   }
-// }
-
-// end state e onPress
-
-
-
-
-
-// class App extends Component {
-//   render() {
-
-//     let nome = 'GUERREIRO'
-//     return (
-//       <View style={styles.container}>
-//         <Text>{nome}</Text>
-//         <Jobs largura={100} altura={200} fulano={'Fulano'} />
-//       </View>
-//     );
-//   }
-// }
-
-
-// // CRIANDO UM COMPONENTE
-// class Jobs extends Component {
-//   render() {
-//     return (
-//       <View>
-//         <Image source={{ uri: 'https://sujeitoprogramador.com/steve.png' }} style={{ width: this.props.largura, height: this.props.altura }} />
-//         <Text>{this.props.fulano}</Text>
-//       </View>
-//     );
-//   }
-// }
