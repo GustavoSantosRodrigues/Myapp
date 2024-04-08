d3d9cb359cf0a412b341e508780607f0b6e8d8cc
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2024-04-08 20:59:57 -0300

    22. App Cronometro

--- App.js
+++ App.js
@@ -1,53 +1,73 @@
-// SERVE PARA CRIAR UM COMPONENTE NO REACT NATIVE
-
 import React, { Component } from "react";
 import { View, Text, StyleSheet, TextInput, Button, Image, TouchableOpacity } from "react-native";
 
-
-// const nome = 'Gustavo'  
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      textoFrase:  'Aproveite o biscoito da sorte',
-      img: require('./src/biscoito.png'),
-    };
+     numero: 0,
+     botao: 'VAI',
+     ultimo: null
+    }
 
-    this.textoQuebraBiscoito = this.textoQuebraBiscoito.bind(this);
-
-    this.frase = [
-      'Aproveite o biscoito da sorte',
-      'Siga os bons e aprenda  com eles',
-      "Acredite em si mesmo. Você é mais capaz do que imagina.",
-      "O sucesso não é o resultado de um acaso, mas sim de uma determinação inabalável.",
-      "Cada novo dia é uma oportunidade para se aproximar dos seus sonhos. Não desperdice.",
-      "As dificuldades de hoje são os degraus para o seu sucesso amanhã.",
-      "Seja a mudança que você deseja ver no mundo. - Mahatma Gandhi",
-      "Não importa o quão devagar você vá, desde que você não pare.",
-      "Você é mais forte do que pensa, mais corajoso do que acredita e mais talentoso do que imagina. - A.A. Milne",
-      "O único lugar onde o sucesso vem antes do trabalho é no dicionário. - Vidal Sassoon",
-      "Nunca é tarde demais para ser quem você quer ser. Não há limite de tempo, comece quando quiser.",
-    ]
+    this.timer = null;
+    this.vai = this.vai.bind(this);
+    this.parar = this.parar.bind(this);
+    this.limpar = this.limpar.bind(this);
   }
 
-  textoQuebraBiscoito() {
-    let numeroAleatorio = Math.floor(Math.random() * this.frase.length)
-    this.setState({
-      textoFrase: ' "' + this.frase[numeroAleatorio] + '" ',
-      img: require('./src/biscoitoAberto.png')
-    })
+  
+
+  vai() {
+    if(this.timer != null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      this.setState({ botao: 'VAI' });
+    }
+    else {
+      this.timer = setInterval(() => {
+        this.setState({ numero: this.state.numero + 0.1 });
+      }, 100);
+    }
+
+    this.setState({ botao: 'PARAR' });
   }
+
+  parar() {
+    
+  }
+  
+  limpar() {
+    if(this.timer != null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    this.setState({ numero: 0, botao: 'VAI', ultimo: this.state.numero });
+  }
+
 
   render() {
     return (
       <View style={styles.container} >
-        <Image source={this.state.img} style={styles.img} />
-        <Text style={styles.textoFrase}>{this.state.textoFrase} </Text>
-        <TouchableOpacity onPress={this.textoQuebraBiscoito}>
-          <View>
-            <Text style={styles.textoQuebraBiscoito}>Quebra o biscoito</Text>
+        <Image source={require('./src/cronometro.png')} style={{ width: 250, height: 250 }} />
+        <Text style={styles.timer}>{this.state.numero.toFixed(2)}</Text>
+
+        <View style={styles.btnArea}>
+          <TouchableOpacity style={styles.btn} onPress={this.vai}>
+            <Text style={styles.btnText}>{this.state.botao}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+            <Text style={styles.btnText}>LIMPAR</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.areaUltimo}>
+            <Text style={styles.ultimo}>
+              {this.state.ultimo != null ? 'ULTIMO: ' + this.state.ultimo.toFixed(2) + 's' : ''}
+            </Text>
           </View>
-        </TouchableOpacity>
+
       </View>
     )
   }
@@ -61,33 +81,39 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  img: {
-    width: 250,
-    height: 250
+  timer: {
+    marginTop: -150,
+    fontSize: 45,
+    fontWeight: 'bold',
   },
 
-  textoFrase: {
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 100,
+    padding: 30,
+  },
+
+
+  btnText: {
     fontSize: 20,
-    color: '#dd7b22',
-    margin: 30,
-    fontStyle: 'italic',
-    textAlign: 'center'
+    fontWeight: 'bold',
+    color: '#FFF',
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: '#000',
+    padding: 10,
+    borderRadius: 5,
   },
 
-  textoQuebraBiscoito: {
-    fontSize: 25,
+  areaUltimo: {
+    marginTop: 20,
+  },
+
+  ultimo: {
+    fontSize: 20,
+    fontWeight: 'bold',
     color: '#000',
-    margin: 30,
-    fontStyle: 'italic',
-    textAlign: 'center',
-    width: 230,
-    height: 50,
-    borderWidth: 2,
-    borderColor: '#dd7b22',
-    borderRadius: 25,
-  },
-
-
+  }
 })
 
 export default App
