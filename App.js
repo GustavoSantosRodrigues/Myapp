ba9eaeb3b24a5abbccfc949d748fb07dac97c2ae
Gustavo Santos <gustavo.rodrigues35@fatec.sp.gov.br>
2024-04-20 21:43:15 -0300

    28 slider: como se fosse para aumentar um volume e diminuir, da pra usar pra kilos tbm e outras tipos de tarefas

--- App.js
+++ App.js
@@ -1,53 +1,40 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, ScrollView, FlatList, SafeAreaView } from "react-native";
 import { Picker } from '@react-native-picker/picker';
+import Slider  from '@react-native-community/slider';
 
 class App extends Component {
-
 
   constructor(props) {
     super(props);
     this.state = {
-      pizza: 0,
-      pizzas: [
-        { key: 1, name: 'Calabresa', preco: 59.90 },
-        { key: 2, name: 'Bauru', preco: 49.90 },
-        { key: 3, name: 'Quatro Queijos', preco: 100.90 },
-        { key: 4, name: 'Marguerita', preco: 10.90 },
-        { key: 5, name: 'Portuguesa', preco: 80.90 },
-        { key: 6, name: 'Napolitana', preco: 67.90 },
-        { key: 7, name: 'Americana', preco: 34.90 },
-        { key: 8, name: 'Espanhola', preco: 1.90 },
-      ]
-    };
+      valor: 50
+    }
   }
 
+ 
+  
 
   render() {
-
-    let pizzasItems = this.state.pizzas.map((v, k) => {
-      return (
-        <Picker.Item key={k} label={v.name} value={k} />
-      )
-    })
 
     return (
       <View style={styles.container} >
 
         <SafeAreaView>
-          <Picker
-            selectedValue={this.state.pizza}
-            onValueChange={(itemValue, itemIndex) => this.setState({ pizza: itemValue })}
-          >
-
-            {pizzasItems}
-          </Picker>
-          <Text style={styles.logo}>Menu Pizza</Text>
-
-          <Text style={styles.pizza}>Você escolheu a: {this.state.pizzas[this.state.pizza].name} </Text>
-          <Text style={styles.preco}>O preço é de R$: {this.state.pizzas[this.state.pizza].preco.toFixed(2)}</Text>
-          {/* <Text>{this.state.pizza}</Text> */}
+          <Slider 
+          minimumValue={0} 
+          maximumValue={100} 
+          onValueChange={(valorSelecionado) => { this.state({ valor: valorSelecionado }) }}
+          value={this.state.valor} 
+          minimumTrackTintColor="#f00"
+          maximumTrackTintColor="#00f"
+          />
+          
         </SafeAreaView>
+
+        <Text>
+          {this.state.valor}
+        </Text>
 
       </View>
     )
@@ -55,33 +42,14 @@ class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ddd',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#ddd',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 
-  logo: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#f00',
-    marginBottom: 20
-  },
-
-  pizza: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#f00',
-    marginBottom: 20
-  },
-
-  preco: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#f00',
-    marginBottom: 20
-  }
+ 
 })
 
 export default App;
